e23211c48250e6731a42aa21bf1c37d89d34d645
lukeMeredith <lukemeredith@protonmail.ch>
2020-09-26 10:14:52 +1000

    added listing funtion to 'read' option

--- journalapp.rb
+++ journalapp.rb
@@ -1,8 +1,11 @@
 require 'date'
-require 'colorize'
+require 'pastel'
 require 'tty-prompt'
 
-prompt = TTY::Prompt.new(active_color: :cyan)
+prompt = TTY::Prompt.new(active_color: :green)
+pastel = Pastel.new
+
+entries = []
 
 def read(filename)
    puts "_________________________________________________________"
@@ -18,7 +21,8 @@ def create(filename, content)
    end
 end
 
-   puts "\n\n    
+puts pastel.green("\n\n
+
                         ▄▄    ▄▄                                                                         ▄▄  
    ▀███▀▀▀██▄           ██  ▀███                  ▀████▀                                               ▀███  
      ██    ▀██▄               ██                    ██                                                   ██  
@@ -28,32 +32,23 @@ end
      ██    ▄██▀█   ██   ██    ██     ███       ███  ██  ██▄   ▄██ ██    ██   ██      ██    ██ ██   ██    ██  
    ▄████████▀ ▀████▀██▄████▄▄████▄   ▄█         █████    ▀█████▀  ▀████▀███▄████▄  ▄████  ████▄████▀██▄▄████▄
                                    ▄█                                                                        
-                                 ██▀                                                                            
-   "                                                                   
+                                 ██▀                                                                        
+                                 
+                                 ")                                                               
 loop do
-   # puts "What would you like to do?"
+   puts "𝐖𝐞𝐥𝐜𝐨𝐦𝐞! 𝐘𝐨𝐮𝐫 𝐎𝐩𝐭𝐢𝐨𝐧𝐬:"
    puts "\n"
-   puts "-New- journal entry"
-   puts "-Read- previous journal entry"
-   puts "-Exit- to close the program"
-   # puts "_________________________________________________________"
+   puts pastel.bright_green.bold("𝐍ew 𝐉𝐨𝐮𝐫𝐧𝐚𝐥 𝐄𝐧𝐭𝐫𝐲")
+   puts pastel.bright_green.bold("𝐑ead 𝐏𝐫𝐞𝐯𝐢𝐨𝐮𝐬 𝐉𝐨𝐮𝐫𝐧𝐚𝐥 𝐄𝐧𝐭𝐫𝐲")
+   puts pastel.bright_green.bold("𝐂lose 𝐃𝐚𝐢𝐥𝐲 𝐉𝐨𝐮𝐫𝐧𝐚𝐥")
    puts "\n"
-   choose = %w(New Read Exit)
+   choose = %w(New Read Close)
    choice = prompt.select("What would you like to do?", choose).to_s
    case choice
-   when "Read"
-      puts "_________________________________________________________"
-      puts "Which note would you like to open?"
-      puts "\n"
-      filename = gets.chomp.to_s.gsub(" ", "_").downcase
-      input
-      filename = "./" + filename + ".txt"
-      read(filename)
-      puts "_________________________________________________________"
    when "New"
       puts "_________________________________________________________"
       puts "\n"
-      puts "Give your note a title:"
+      puts "Give your journal entry a title:"
       puts "\n"
       filename = gets.chomp.to_s.gsub(" ", "_").downcase
       filename = filename + ".txt"
@@ -67,7 +62,21 @@ loop do
       create(filename, content)
       puts "\n"
       puts "Note created!"
-   when "Exit"
+      # entries.push(filename)
+      File.open("./entries.txt", "w") do |file|
+         file.write(filename)
+      end
+   when "Read"
+      # entries.each {|entry| puts "Title: #{entry}"}
+      read("./entries.txt")
+      puts "_________________________________________________________"
+      puts "Which journal entry would you like to read?"
+      puts "\n"
+      filename = gets.chomp.to_s.gsub(" ", "_").downcase
+      filename = "./" + filename + ".txt"
+      read(filename)
+      puts "_________________________________________________________"
+   when "Close"
       break
    else
       puts "Yo, that's not a valid choice."
