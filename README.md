82e4714771223e0b2dfee665150e85f652d45574
lukeMeredith <lukemeredith@protonmail.ch>
2020-09-29 13:38:17 +1000

    refactored some code, began work on the README file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Assignment 3 - Terminal App
+# Assignment 3 - Terminal App - SUPERNOTES
 
 
 ## R1
@@ -14,4 +14,28 @@ what?
 
 
 ## R5 - Software Development Plan
+
+- My application, SuperNotes, is a terminal application designed to do three things: take notes, read those note, and delete those notes if necessary. SuperNote takes input from a user in the form of a note title and note content, then opens a text file and saves the user's input (the note heading and content) to the text file along with the date which the note was created. The user can also use SuperNote to look through a list of their created notes, or search for them by name, and either edit the note or delete the note. Basically, SuperNote is super basic. But it works, and it's super. 
+- It will solve all the client's note taking needs; even though there are hundreds of other note-taking apps on the market, you can rest assured that none of them take a note as SUPERbly as SuperNote!
+- The target audience is anyone who wants to jump in on the fast-paced and always-expanding world of note-taking. From office workers to soccer-mums, if you need to take a note, we got the app for you!
+- I could explain how to use it here, under the Requirement 5 heading as asked, but I feel like that's a bit silly as Requirement 10 asks for the same thing, and I don't want to repeat myself. I got things to do. 
+
+## R6
+
+### Feature 1 - Create New Note
+
+The first and most essential feature of SuperNote is it's ability to create a new note. When choosing th *New* option, the user is prompted for a note title (which can be multiple words, as the app will fill in whitespace with underscores'_' and add the '.txt' file extention automatically). If the note title is already in use, the app will tell the user that the title is unavailable. After successfully giving the note a title, the user then iputs the content of their note, and upon saving, the note is transformed into a text file, with a header that includes the owner of the note, the note title, and the date which the note was created. 
+
+### Feature 2 - Read Your Notes
+
+The second feature is, of course, the ability to read your notes. The *Read* option lists all created notes. Rather than having to scroll up and down through the list of notes, the user can instead open the note by simply typing in the note's title (again, no need to add the '.txt' extension and any underscores '_').
+
+### Feature 3  - Edit Your Notes
+
+The third feature gives the user the ability to edit pre-existing notes. Pretty simple.
+
+### Feature 4 - Delete Unwanted Notes
+
+The final feature of SuperNote is the ability to list all existing notes, search by name (again, sans underscores'_' and '.txt' file extensions) and delete them from the list. It's pretty super.
+
 
---
+++ supernote.rb
@@ -0,0 +1,169 @@
+require 'date'
+require 'pastel'
+require 'tty-prompt'
+
+prompt = TTY::Prompt.new(active_color: :yellow)
+pastel = Pastel.new
+
+def read(filename)
+   File.open(filename).each do |text|
+   puts text
+   end
+end
+
+def create(filename, content)
+   File.open(filename, "w") do |file|     
+   file.write(content)
+   end
+end
+
+def note_list()
+   pastel = Pastel.new
+   puts pastel.yellow.underline("\nNote List:__________________________________________________________")
+   puts "\n"
+   puts Dir.glob("*.txt")
+   puts "\n"
+end
+
+def line()
+   pastel = Pastel.new
+   puts pastel.yellow.underline"________________________________________________________________________________________"
+end
+
+def title
+   pastel = Pastel.new
+   puts pastel.yellow("\n\n                           SUPER                        
+   ▀███▄   ▀███▀ ▄▄█▀▀██▄ ███▀▀██▀▀██████▀▀▀███ ▄█▀▀▀█▄██ 
+     ███▄    █ ▄██▀    ▀██▄▀   ██   ▀█ ██    ▀█▄██    ▀██ 
+     █ ███   █ ██▀      ▀██    ██      ██   █  ▀███▄   ██ 
+     █  ▀██▄ █ ██        ██    ██      ██████    ▀█████▄█ 
+     █   ▀██▄█ ██▄      ▄██    ██      ██   █  ▄     ▀██▀ 
+     █     ███ ▀██▄    ▄██▀    ██      ██     ▄██     ██▄ 
+   ▄███▄    ██   ▀▀████▀▀    ▄████▄  ▄██████████▀█████▀██ 
+      
+      NotES!      notesss         NOTES           nOteS.
+
+            notes!         notes!         notes!
+  
+                     Welcome to SUPERNOTES
+
+A SUPER app for taking HEAPS of NOTES and absolutely f*cking NOTHING else.
+
+                        **finger guns**")
+end
+
+title()
+
+loop do
+
+   puts pastel.yellow.underline("\n𝐘𝐨𝐮𝐫 𝐎𝐩𝐭𝐢𝐨𝐧𝐬:_______________________________________________________")
+   puts "\n"
+   puts pastel.yellow("𝐑ead 𝐍𝐨𝐭𝐞 | 𝐍ew 𝐍𝐨𝐭𝐞 | 𝐃𝐞𝐥𝐞𝐭𝐞 𝐍𝐨𝐭𝐞 | 𝐄𝐝𝐢𝐭 𝐍𝐨𝐭𝐞 | 𝐄𝐱𝐢𝐭")
+   puts "\n"
+  
+   choose = %w(Read New Delete Exit)
+   choice = prompt.select("What would you like to do?", choose).to_s
+
+   case choice
+      when "New"
+         line()
+         puts pastel.yellow("\nGive your note a title:")
+         puts "\n"
+         filename = gets.chomp.to_s.gsub(" ", "_").downcase
+         filename = filename + ".txt"
+         if File.exists?(filename) == false
+            puts "\n"
+            line()
+            puts pastel.yellow("\nNow add something to " + "#{filename}:")
+            puts "\n"
+            date = Date.today
+            content = "Title: #{filename}" + "\nDate: #{date}" + "\n\n" + gets.chomp.to_s
+            create(filename, content)
+            puts pastel.bright_green("\nNote created!")
+            puts "\n"
+            choose2nd = %w(Back)
+            choice2nd = prompt.select("Go Back?", choose2nd).to_s         
+               case choice2nd
+               when "Back"
+                  puts "\n"
+                  title()                
+               else 
+                  puts pastel.red("Error!")
+               end
+         else 
+            puts "\n"
+            puts pastel.red("Hey man. So, bad news, this note title already exists...")
+         end
+      
+      when "Read"
+         note_list()
+         puts pastel.yellow("\nWhich note entry would you like to read?")
+         puts pastel.yellow("\nType in the note's title below, and don't worry about the file extension (.txt) or the underscore!")
+         puts pastel.yellow("(Leave blank and press 'Enter' to go back)")
+         puts "\n"
+         filename = gets.chomp.to_s.gsub(" ", "_").downcase
+         filename = filename + ".txt"
+         loop do
+            if File.exists?(filename) == true
+               line()
+               puts "\n"
+               read(filename)
+               line()
+                  choose2nd = %w(Back)
+                  choice2nd = prompt.select("Go Back?", choose2nd).to_s         
+                  case choice2nd
+                  when "Back"
+                     puts "\n"
+                     title()
+                     break                 
+                  else 
+                     puts pastel.red("Error!")
+                  end
+            else
+               puts pastel.red.bold("\nYo, that note doesn't exist...")
+               break
+            end  
+         end
+
+      when "Delete"
+         note_list()
+         puts pastel.yellow("\nWhich note entry would you like to delete?")
+         puts pastel.yellow("Type in the note's title below, and don't worry about the file extension (.txt) or the underscore!")
+         puts "\n"
+         filename = gets.chomp.to_s.gsub(" ", "_").downcase
+         filename = filename + ".txt"
+            if File.exists?(filename) == true
+               File.delete(filename)
+               puts pastel.red("\nNote deleted!")
+               line()
+               choose2nd = %w(Back)
+               choice2nd = prompt.select("Go Back?", choose2nd).to_s         
+               case choice2nd
+               when "Back"
+                  puts "\n"
+                  title()                
+               else 
+                  puts pastel.red("Error!")
+               end
+            else
+               puts pastel.red.bold("Bruh, you can't delete it if it doesn't exist...")
+               choose2nd = %w(Back)
+               choice2nd = prompt.select("Go Back?", choose2nd).to_s         
+               case choice2nd
+               when "Back"
+                  puts "\n"
+                  title()                
+               else 
+                  puts pastel.red("Error!")
+               end
+            end
+      when "Exit"
+         puts "\n"
+         puts pastel.yellow("\nHave a SUPER day!")
+         puts "\n"
+         break
+      
+      else
+         puts pastel.red("\nYo, that's not a valid choice. Try again...?")
+      end
+end
